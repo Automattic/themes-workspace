3c0eac6ad423b3e08d32174ac05660eb086ea7fb
Allan Cole <allan.cole@automattic.com>
2020-02-18 17:38:16 -0500

    Varya: Reconnect comments styles.

--- varya/comments.php
+++ varya/comments.php
@@ -32,13 +32,13 @@ if ( post_password_required() ) {
 			if ( '1' === $varya_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'varia' ),
+					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'varya' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $varya_comment_count, 'comments title', 'varia' ) ),
+					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $varya_comment_count, 'comments title', 'varya' ) ),
 					number_format_i18n( $varya_comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
@@ -63,7 +63,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'varia' ); ?></p>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'varya' ); ?></p>
 			<?php
 		endif;
 
