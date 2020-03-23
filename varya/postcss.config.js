4dcbd799c9ef476b6ff224265ab84b4ae126cd6b
Jeffrey Ong <jeff.ong@automattic.com>
2020-03-23 15:53:46 -0400

    Require autoprefixer

--- varya/postcss.config.js
+++ varya/postcss.config.js
@@ -1,17 +1,18 @@
+const autoprefixer = require('autoprefixer');
 const postcssFocusWithin = require('postcss-focus-within');
 const postcssCustomMedia = require('postcss-custom-media');
 const postcssCustomProperties = require('postcss-custom-properties');
 
 module.exports = {
-	plugins: [
+  plugins: [
     autoprefixer(),
-		postcssFocusWithin(/* pluginOptions */),
-		postcssCustomMedia(/* pluginOptions */),
+    postcssFocusWithin(),
+    postcssCustomMedia(),
     postcssCustomProperties({
       importFrom: [
         'assets/css/variables.css',
         'assets/css/variables-editor.css'
       ]
     })
-	]
+  ]
 };
