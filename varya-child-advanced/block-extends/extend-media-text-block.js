903e62e12e22f631183e72c39a4fc544afb9948f
Jeff Ong <jonger4@gmail.com>
2020-03-23 11:48:59 -0400

    Revert "Varya Theme: Adding a css-variables version of the original version of Varia."

--- varya-child-advanced/block-extends/extend-media-text-block.js
+++
@@ -1,6 +0,0 @@
-( function( blocks ) {
-	blocks.registerBlockStyle( 'core/media-text', {
-		name: 'media-text-hero',
-		label: 'Hero'
-	} );
-}( window.wp.blocks ) );
--- varya-child-advanced/block-extends/extend-quote-block.js
+++
@@ -1,6 +0,0 @@
-( function( blocks ) {
-	blocks.registerBlockStyle( 'core/quote', {
-		name: 'quote-blobs',
-		label: 'Blobs'
-	} );
-}( window.wp.blocks ) );
