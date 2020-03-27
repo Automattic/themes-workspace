254756303cb6ca0b9f02d892d5070240ae667f23
Kjell Reigstad <kjell@kjellr.com>
2020-03-26 22:34:14 -0400

    Add basic block templates.

--- varya-blocks/functions.php
+++ varya-blocks/functions.php
@@ -9,3 +9,10 @@
  * @since 1.0.0
  */
 
+/**
+ * Enqueue scripts and styles.
+ */
+function varya_blocks_enqueue() {
+	wp_enqueue_style( 'varya-blocks-styles', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'varya_blocks_enqueue' );
