0b48f8341c75c3135a8cf754c9688335093a6dbc
Kjell Reigstad <kjell@kjellr.com>
2020-03-26 22:17:25 -0400

    Add basic child theme.

---
+++ varya-blocks/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Varya Blocks functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage varya_blocks
+ * @since 1.0.0
+ */
+
