e4ee444dd887f5948d134ea4e17e77a3a57c4d84
Allan Cole <allan.cole@automattic.com>
2020-04-09 13:25:51 -0400

    Varya: Clean up toggle button text for better open/close clarity.

--- varya/inc/woocommerce.php
+++ varya/inc/woocommerce.php
@@ -217,19 +217,19 @@ function varya_add_cart_menu( $nav, $args ) {
 			'%1$s
 			</ul></div>
 			<input type="checkbox" role="button" aria-haspopup="true" id="woocommerce-toggle" class="hide-visually">
-			<button id="toggle-cart" class="button">%2$s %3$s
-				<span class="dropdown-icon open">%4$s</span>
-				<span class="dropdown-icon close">%5$s</span>
-				<span class="hide-visually expanded-text">%6$s</span>
-				<span class="hide-visually collapsed-text">%7$s</span>
+			<button id="toggle-cart" class="button">
+				<span class="dropdown-icon open">%2$s %3$s %4$s</span>
+				<span class="dropdown-icon close">%5$s %6$s</span>
+				<span class="hide-visually expanded-text">%7$s</span>
+				<span class="hide-visually collapsed-text">%8$s</span>
 			</button>
 			<div class="woocommerce-menu-container">
 			<ul id="woocommerce-menu" class="main-menu" aria-label="submenu">
-			<li class="menu-item woocommerce-menu-item %8$s" title="%9$s">
-				%10$s
+			<li class="menu-item woocommerce-menu-item %9$s" title="%10$s">
+				%11$s
 				<ul class="sub-menu">
-					<li class="woocommerce-cart-widget" title="%11$s">
-						%12$s
+					<li class="woocommerce-cart-widget" title="%12$s">
+						%13$s
 					</li>
 				</ul>
 			</li>',
@@ -237,6 +237,7 @@ function varya_add_cart_menu( $nav, $args ) {
 			varya_get_icon_svg( 'shopping_cart', 16 ),
 			esc_html__( 'Cart', 'varya' ),
 			varya_get_icon_svg( 'menu_open' ),
+			esc_html__( 'Close', 'varya' ),
 			varya_get_icon_svg( 'menu_close' ),
 			esc_html__( 'expanded', 'varya' ),
 			esc_html__( 'collapsed', 'varya' ),
