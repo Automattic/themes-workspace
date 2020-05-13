2461c9a8df0f85d905da50d4cd5142e3b790c195
Jeffrey Ong <jeff.ong@automattic.com>
2020-05-13 10:34:08 -0400

    Duotone style variations for gallery and cover block.

--- sk8prk/assets/block-extends/extend-blocks.js
+++ sk8prk/assets/block-extends/extend-blocks.js
@@ -1,7 +1,30 @@
 /* global wp */
 ( function( blocks ) {
-    blocks.registerBlockStyle( 'core/group', {
-        name: 'outline',
-        label: 'Outline'
-    } );
+    var styles = [
+        {
+            name: "core/group",
+            options: {
+                name: "outline",
+                label: "Outline",
+            },
+        },
+        {
+            name: "core/cover",
+            options: {
+                name: "duotone",
+                label: "Duotone",
+            },
+        },
+        {
+            name: "core/gallery",
+            options: {
+                name: "duotone",
+                label: "Duotone",
+            },
+        },
+    ];
+    
+    styles.forEach ( function(style){
+        blocks.registerBlockStyle( style.name, style.options );
+    });
 }( wp.blocks ) );
